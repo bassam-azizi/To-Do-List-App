0e4891b8dedaadcf085510d23df41af2ed6193e9
BassamAzizi <101010bassam@gmail.com>
2019-12-31 22:07:13 +0100

    finishing the to-do-app-list logic

---
+++ src/app.js
@@ -0,0 +1,74 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newItem: "",
+      list: []
+    };
+  }
+  updateItems(key, value) {
+    // update React State
+    this.setState({
+      [key]: value
+    });
+  }
+  addItems() {
+    // create the new item with a unique id
+    const newItem = {
+      id: 1 + Math.random(),
+      value: this.state.newItem.slice()
+    };
+    // coppy the current list of items
+    const list = [...this.state.list];
+
+    // add new items to list
+    list.push(newItem);
+
+    // update the list and reset the new item to an empty string for new input
+    this.setState({ list, newItem: "" });
+  }
+
+  deleteItem(id) {
+    const list = [...this.state.list];
+    const updatedList = list.filter(item => item.id !== id);
+
+    this.setState({ list: updatedList });
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <h2>Add items here..</h2>
+        <input
+          className="appInput"
+          type="text"
+          placeholder="Add items here.."
+          value={this.state.newItem}
+          onChange={e => this.updateItems("newItem", e.target.value)}
+        />
+        <button className="appButton" onClick={() => this.addItems()}>
+          Add
+        </button>
+        <div className="itemAdded">
+          {this.state.list.map(item => {
+            return (
+              <li key={item.id}>
+                {item.value}
+                <button
+                  className="deleteItem"
+                  onClick={() => this.deleteItem(item.id)}
+                >
+                  X
+                </button>
+              </li>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import App from "./app";
 import "./styles.css";
-
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
-  );
-}
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
